93499b85a0a86a22ab84ac51ee2dd61128ea414f
Gabriel <rgabriel1508@gmail.com>
2021-03-06 19:53:30 +0530

    working on home_screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,19 @@ import './pages/logo_screen.dart';
 import './pages/scan_screen.dart';
 import './pages/home_screen.dart';
 import './pages/result_screen.dart';
+// import './pages/camera_test.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "ScanDoc",
-    theme: ThemeData(backgroundColor: Colors.white),
+    theme: ThemeData(primaryColor: Colors.black),
     initialRoute: 'scan_screen',
     routes: {
       'logo_screen': (context) => LogoScreen(),
       'scan_screen': (context) => ScanScreen(),
       'result_screen': (context) => ResultScreen(),
       'home_screen': (context) => HomeScreen(),
+      // 'camera_test': (context) => CameraApp()
     },
   ));
 }
--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -9,7 +9,46 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Home Screen"),
+      appBar: AppBar(
+        title: Text("Scan Doc"),
+      ),
+      body: Container(
+        color: Colors.black,
+        child: ListView(
+          children: [
+            DocumentTile(
+              title: "IA1_1019121_EM_Q2",
+              pages: 10,
+              date: "3rd Jan 2021",
+            ),
+            DocumentTile(
+              title: "IA1_1019121_EM_Q2",
+              pages: 10,
+              date: "3rd Jan 2021",
+            ),
+            DocumentTile(
+              title: "IA1_1019121_EM_Q2",
+              pages: 10,
+              date: "3rd Jan 2021",
+            ),
+            DocumentTile(
+              title: "IA1_1019121_EM_Q2",
+              pages: 10,
+              date: "3rd Jan 2021",
+            ),
+            DocumentTile(
+              title: "IA1_1019121_EM_Q2",
+              pages: 10,
+              date: "3rd Jan 2021",
+            ),
+            DocumentTile(
+              title: "IA1_1019121_EM_Q2",
+              pages: 10,
+              date: "3rd Jan 2021",
+            )
+          ],
+        ),
+      ),
       floatingActionButton: Container(
         height: 70,
         width: 80,
@@ -24,6 +63,115 @@ class _HomeScreenState extends State<HomeScreen> {
             size: 30,
           ),
         ),
+      ),
+    );
+  }
+}
+
+class DocumentTile extends StatefulWidget {
+  final String title;
+  final int pages;
+  final String date;
+
+  DocumentTile({this.title, this.pages, this.date});
+
+  @override
+  _DocumentTileState createState() => _DocumentTileState();
+}
+
+class _DocumentTileState extends State<DocumentTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.grey[800], borderRadius: BorderRadius.circular(5.0)),
+      height: 200,
+      padding: EdgeInsets.fromLTRB(10.0, 10.0, 0.0, 10.0),
+      margin: EdgeInsets.all(10.0),
+      child: Row(
+        children: [
+          Container(
+            child: Placeholder(
+              fallbackHeight: 200,
+              fallbackWidth: 130,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding:
+                  const EdgeInsets.only(left: 10.0, bottom: 5.0, top: 12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Row(
+                        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        // children: [
+                        Text(
+                          this.widget.title,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                        // IconButton(
+                        //   onPressed: () {},
+                        //   icon: Icon(Icons.more_vert),
+                        // )
+                        // ],
+                        // ),
+                        const Padding(padding: EdgeInsets.only(bottom: 2.0)),
+                        Text(
+                          this.widget.pages.toString() + " pages",
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          this.widget.date,
+                          style: const TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.white70,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.more_vert,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
